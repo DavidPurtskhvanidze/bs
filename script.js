719573784a05bd514ea0d7f55eb6aa798226162e
david <david.purtskhvanidze@gmail.com>
2024-10-09 23:42:37 +0600

    Added facts section

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const riskSlider = new Swiper('#risk_slider', {
+const riskSlider = new Swiper('#risk-slider', {
     loop: true,
     // Default parameters
     slidesPerView: 6,
+    spaceBetween: 20,
+    // Responsive breakpoints
+    // breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30
+    //     },
+    //     // when window width is >= 640px
+    //     640: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40
+    //     }
+    // }
+});
+const factsSlider = new Swiper('#facts-slider', {
+    loop: true,
+    // Default parameters
+    slidesPerView: 4,
     spaceBetween: 20,
     // Responsive breakpoints
     // breakpoints: {
