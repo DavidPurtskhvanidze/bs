cf3a36ef7c02a1ecd232dc1254a8438a6f369092
david <david.purtskhvanidze@gmail.com>
2024-11-04 18:49:57 +0600

    sg added script for scroll

---
+++ sleep_and_glow/sg_shop/new_files/script.js
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll(".goods-grid");
+const navLinks = document.querySelectorAll(".shop-section__goods_item");
+function highlightMenu() {
+    let scrollPosition = window.scrollY + window.innerHeight / 2;
+
+    sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollPosition - 200 >= sectionTop && scrollPosition - 200 < sectionTop + sectionHeight) {
+            navLinks.forEach(link => link.classList.remove("active"));
+            navLinks[index].classList.add("active");
+        }
+    });
+}
+window.addEventListener("scroll", highlightMenu);
+
+navLinks.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute("data-id");
+        const targetSection = document.getElementById(targetId);
+        const shopSection = document.getElementsByClassName("shop-section");
+        window.scrollTo({
+            top: targetSection.offsetTop + (shopSection[0].offsetTop - 190),
+            behavior: "smooth"
+        });
+    });
+});
