bd1bd121cca667f6ea63e4f8269a1bcb0e7bb306
david <david.purtskhvanidze@gmail.com>
2025-05-02 20:23:57 +0600

    BS catalog mobile menu/ sizes

--- beauty_sleep/bs_shop_prod/bs_shop_prod_files/catalog.js
+++ beauty_sleep/bs_shop_prod/bs_shop_prod_files/catalog.js
@@ -1,10 +1,20 @@
 const sections = document.querySelectorAll(".goods-grid");
 const navLinks = document.querySelectorAll(".shop-section__goods_item_action");
 const seenSections = new Set();
+const mainHeader = $('header.header');
+const shopSidebarBlock = $('.shop-section .shop-section__goods_sidebar > div');
 
+function sidebarTopPosition() {
+    if (mainHeader.hasClass('hide')) {
+        shopSidebarBlock.css('top', 0);
+    } else {
+        shopSidebarBlock.css('top', mainHeader.height());
+    }
+}
+sidebarTopPosition();
 function highlightMenu() {
     let scrollPosition = window.scrollY + window.innerHeight / 2;
-
+    sidebarTopPosition()
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.offsetHeight;
@@ -48,22 +58,81 @@ navLinks.forEach(link => {
 });
 
 $(document).ready(function() {
-	$('.js-product-color').each(function(index) {
+    $('.js-product-color').each(function(index) {
         const label = $(this).find('label');
-	    const labelCount = label.length;
+        const colorOverlay = $(this).find('.color-overlay');
+        const labelCount = label.length;
         label.each(function(index) {
             $(this).addClass('lb-color-' + (index + 1));
         });
-	    if (labelCount > 5) {
-	    	$(this).addClass("collapse");
+        if (labelCount > 5) {
+            $(this).addClass("collapse");
             $(this).find('.slide-product__img_colors').append('<div class="more-color-count">'+ (labelCount - 4) +'</div>');
-	    }
-	});
+
+            const form = $(this).closest('form');
+            const oldBtn = form.find('.btn-add-basket');
+            const newBtn = oldBtn.clone().attr({
+                type: 'button',
+                name: 'ms2_action2',
+                value: 'cart/add2'
+            }).removeAttr('onclick');
+
+            newBtn.find('span').text('опции');
+            oldBtn.replaceWith(newBtn);
 
 
-	$(".js-product-color.collapse").click(function() {
-		const productColor = $(this).closest(".js-product-color");
-	    productColor.removeClass("collapse");
-	    productColor.find(".color-overlay").hide();
- 	})
+        } else {
+            console.log("hidden");
+            colorOverlay.hide();
+        }
+    });
+
+
+    $(".js-product-color.collapse").click(function() {
+        const productColor = $(this).closest(".js-product-color");
+        productColor.removeClass("collapse");
+        productColor.find(".color-overlay").hide();
+    })
+
+    $(".btn-add-basket[type=button]").click(function() {
+        $(this)
+            .attr('type', 'submit')
+            .attr('name', 'ms2_action')
+            .attr('value', 'cart/add')
+            .off('click') // Удаляет текущий click, чтобы не зациклить
+            .on('click', function() {
+                addToCart(this);
+            });
+
+        $(this).find('span').text('добавить');
+        const form = $(this).closest('form');
+        const productColor = form.find(".js-product-color");
+        productColor.removeClass("collapse");
+        productColor.find(".color-overlay").hide();
+    });
+
+    $("a.btn-add-basket").click(function() {
+        const oldBtn = $(this);
+
+        const newBtn = $('<button>', {
+            class: oldBtn.attr('class'),
+            type: 'button',
+            name: 'ms2_action2',
+            value: 'cart/add2',
+            'data-reachgoal': oldBtn.attr('data-reachgoal'),
+            'data-pagetitle': oldBtn.attr('data-pagetitle'),
+            'data-parent': oldBtn.attr('data-parent'),
+            'data-id': oldBtn.attr('data-id'),
+            'data-price': oldBtn.attr('data-price'),
+            style: oldBtn.attr('style')
+        }).append(oldBtn.contents()); // переносим <span>, <svg> и т.п.
+
+        oldBtn.replaceWith(newBtn);
+
+        oldBtn.find('span').text('добавить');
+        const form = $(this).closest('form');
+        const productColor = form.find(".js-product-color");
+        productColor.removeClass("collapse");
+        productColor.find(".color-overlay").hide();
+    });
 })
